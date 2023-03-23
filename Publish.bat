a4d05de043f30c3edc10e7ec947cdc997c3b1e82
Peter Morris <mrpmorris@gmail.com>
2023-03-23 19:34:41 +0000

    Update version to 1.0

--- Publish.bat
+++ Publish.bat
@@ -1,5 +1,5 @@
 cls
-@echo **** 0.1.0-Alpha1 : UPDATED THE VERSION NUMBER IN THE PROJECT *AND* BATCH FILE? ****
+@echo **** 1.0.0 : UPDATED THE VERSION NUMBER IN THE PROJECT *AND* BATCH FILE? ****
 pause
 
 cls
@@ -10,9 +10,9 @@ cls
 @echo
 @echo ======================
 
-set /p ShouldPublish=Publish 0.1.0-Alpha1 [yes]?
+set /p ShouldPublish=Publish 1.0.0 [yes]?
 @if "%ShouldPublish%" == "yes" (
 	@echo PUBLISHING
-	dotnet nuget push .\Source\nugets\MetaMerge.Fody.0.1.0-Alpha1.nupkg -k %MORRIS.NUGET.KEY% -s https://api.nuget.org/v3/index.json
+	dotnet nuget push .\Source\nugets\MetaMerge.Fody.1.0.0.nupkg -k %MORRIS.NUGET.KEY% -s https://api.nuget.org/v3/index.json
 )
 
