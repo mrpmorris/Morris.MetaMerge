d0338db8b96f313f33a9e27e9dbe9acb50b65b2b
Peter Morris <mrpmorris@gmail.com>
2023-03-23 18:07:28 +0000

    WIP

---
+++ Source/Tutorials/01-SyncAttributes/SyncAttributesTutorial.Contracts/PersonCreateCommand.cs
@@ -0,0 +1,16 @@
+﻿using MetaMerge;
+using SyncAttributesTutorial.Definitions;
+
+namespace SyncAttributesTutorial.Contracts;
+
+public class PersonCreateCommand
+{
+	[Meta(typeof(PersonDefinitions.Salutation))]
+	public string Salutation { get; set; }
+
+	[Meta(typeof(PersonDefinitions.GivenName))]
+	public string GivenName { get; set; }
+
+	[Meta(typeof(PersonDefinitions.FamilyName))]
+	public string FamilyName { get; set; }
+}
---
+++ Source/Tutorials/01-SyncAttributes/SyncAttributesTutorial.Contracts/PersonUpdateCommand.cs
@@ -0,0 +1,19 @@
+﻿using MetaMerge;
+using SyncAttributesTutorial.Definitions;
+using System;
+
+namespace SyncAttributesTutorial.Contracts;
+
+public class PersonUpdateCommand
+{
+	public Guid Id { get; set; }
+
+	[Meta(typeof(PersonDefinitions.Salutation))]
+	public string Salutation { get; set; }
+
+	[Meta(typeof(PersonDefinitions.GivenName))]
+	public string GivenName { get; set; }
+
+	[Meta(typeof(PersonDefinitions.FamilyName))]
+	public string FamilyName { get; set; }
+}
